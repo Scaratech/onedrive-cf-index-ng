18c889370358957461d53c4e0206a18593465557
Scaratek <153395462+Scaratech@users.noreply.github.com>
2025-03-20 09:42:17 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,8 @@ class MyDocument extends Document {
         <Head>
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://dl.korone.cloud" />
-          <meta property="og:title" content="ChromeOS Mirror" />
-          <meta property="og:description" content="Scaratek's ChromeOS Download Mirror" />
+          <meta property="og:title" content="Korone Cloud CDN" />
+          <meta property="og:description" content="Korone Cloud CDN" />
           <meta property="og:image" content="https://dl.korone.cloud/icons/128.png" />
           <meta name="theme-color" content="#f9e2af" />
           <link rel="icon" href="/favicon.ico" />
