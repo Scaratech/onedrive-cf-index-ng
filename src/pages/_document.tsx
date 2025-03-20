7b30c5e661a8a370e0755df90b86cc6363360052
Scaratek <153395462+Scaratech@users.noreply.github.com>
2025-03-20 09:43:42 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,10 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://dl.korone.cloud" />
+          <meta property="og:url" content="https://cdn.korone.cloud" />
           <meta property="og:title" content="Korone Cloud CDN" />
           <meta property="og:description" content="Korone Cloud CDN" />
-          <meta property="og:image" content="https://dl.korone.cloud/icons/128.png" />
+          <meta property="og:image" content="https://cdn.korone.cloud/icons/128.png" />
           <meta name="theme-color" content="#f9e2af" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
